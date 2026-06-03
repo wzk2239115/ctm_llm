d9943b2ffe76e472ce2e73e1a52d73cf6ae4b096
wzk2239115 <wzk2239115@gmail.com>
2026-06-03 14:07:46 +0800

    scale up model: 108M params, 30 iters, 8xH100 config

--- model/config.py
+++ model/config.py
@@ -1,25 +1,25 @@
 class CTMLLMConfig:
     def __init__(self, **kwargs):
         self.vocab_size = kwargs.get('vocab_size', 6400)
-        self.hidden_size = kwargs.get('hidden_size', 512)
+        self.hidden_size = kwargs.get('hidden_size', 768)
         self.max_position_embeddings = kwargs.get('max_position_embeddings', 2048)
         self.dropout = kwargs.get('dropout', 0.0)
         self.rms_norm_eps = kwargs.get('rms_norm_eps', 1e-6)
 
-        self.d_model = kwargs.get('d_model', 256)
-        self.d_input = kwargs.get('d_input', 128)
-        self.iterations = kwargs.get('iterations', 3)
-        self.memory_length = kwargs.get('memory_length', 5)
-        self.heads = kwargs.get('heads', 4)
-        self.n_synch_out = kwargs.get('n_synch_out', 256)
-        self.n_synch_action = kwargs.get('n_synch_action', 256)
-        self.synapse_depth = kwargs.get('synapse_depth', 2)
+        self.d_model = kwargs.get('d_model', 512)
+        self.d_input = kwargs.get('d_input', 256)
+        self.iterations = kwargs.get('iterations', 30)
+        self.memory_length = kwargs.get('memory_length', 10)
+        self.heads = kwargs.get('heads', 8)
+        self.n_synch_out = kwargs.get('n_synch_out', 512)
+        self.n_synch_action = kwargs.get('n_synch_action', 512)
+        self.synapse_depth = kwargs.get('synapse_depth', 3)
         self.deep_nlms = kwargs.get('deep_nlms', True)
         self.memory_hidden_dims = kwargs.get('memory_hidden_dims', 4)
         self.neuron_select_type = kwargs.get('neuron_select_type', 'random-pairing')
         self.n_random_pairing_self = kwargs.get('n_random_pairing_self', 0)
 
-        self.num_hidden_layers = kwargs.get('num_hidden_layers', 8)
+        self.num_hidden_layers = kwargs.get('num_hidden_layers', 12)
         self.tie_word_embeddings = kwargs.get('tie_word_embeddings', True)
 
         assert self.d_model >= max(self.n_synch_out, self.n_synch_action), \
